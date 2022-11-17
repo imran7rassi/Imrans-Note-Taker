3e3c25d48add2f690eb6406453c14ab91cb58f2f
Imran rassi <imran.rassi.2020@gmail.com>
2022-11-17 18:48:23 -0500

    update some commits

--- server.js
+++ server.js
@@ -1,25 +1,36 @@
+
+// use the express to require the data //
+
 const express = require('express');
 
-// app use express
+// we use the express for the app //
+
 const app = express();
 
-// creating environment variable port
+// creating the port for the variables //
 const PORT = process.env.PORT || 4050;
 
 
-// asks express to create a route for every file in the 'public' folder and give it a '/' route
+// asks express to create a route for every
+//file in the 'public' folder //
+
 app.use(express.static('public'));
-// sets up express app to handel data parser, middle wear created req.body
+
+// sets up express app to handel data parser,
+// middle wear created req.body
+
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 
-// routes to route files
+// adding the routes from our routing files //
+
 require('./routes/apiRoutes')(app);
 require('./routes/htmlRoutes')(app);
 
 
-// app listener - starts the server
+// adding app listener to starts the server //
+
 app.listen(PORT, () => {
   console.log(`The Server is available at localhost:${PORT}`);
 });
